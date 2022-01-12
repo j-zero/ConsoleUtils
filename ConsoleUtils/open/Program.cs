1a2494f5beb11639a59c1d30e33af2052585ca3e
unknown <er@j-zero.de>
2022-01-12 19:33:54 +0100

    initial commit

---
+++ ConsoleUtils/open/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace open
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                System.Diagnostics.Process.Start(args[0]);
+            else
+                System.Diagnostics.Process.Start(Environment.CurrentDirectory);
+        }
+    }
+}

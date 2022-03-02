008ba5a9babfd403d5c8352d306a52d72901b01c
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2022-03-02 16:00:13 +0100

    open: added usage, added create file.

--- ConsoleUtils/open/Program.cs
+++ ConsoleUtils/open/Program.cs
@@ -5,8 +5,34 @@ static class Program
     [STAThread]
     static void Main(string[] args)
     {
-        if (args.Length > 0)
-            System.Diagnostics.Process.Start(args[0]);
+        if (args.Length > 0) {
+            string f = args[0];
+            if (f == "--help")
+            {
+                Console.Write($"Usage: open [file|url]\nIf no parameter is given, the current directory will be opened.");
+                return;
+            }
+            else
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(f);
+                }
+                catch (System.ComponentModel.Win32Exception ex)
+                {
+                    if (ex.NativeErrorCode == 0x02) //https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/18d8fbe8-a967-4f1c-ae50-99ca8e491d2d
+                    {
+                        Console.Write($"File \"{f}\" not found, create? [y/N]");
+                        ConsoleKey response = Console.ReadKey(false).Key;
+                        if (response == ConsoleKey.Y)
+                        {
+                            System.IO.File.Create(f);
+                            System.Diagnostics.Process.Start(f);
+                        }
+                    }
+                }
+            }
+        }
         else
             System.Diagnostics.Process.Start(Environment.CurrentDirectory);
     }

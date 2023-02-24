a5394b4825394ff693fa823af0c301d79353d348
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2023-02-24 22:03:22 +0100

    bugfix

---
+++ ConsoleUtils/ConsoleUtilsCore/DynamicHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+
+public class DynamicHelper
+{
+    public static bool HasProperty(dynamic dyn, string propertyname)
+    {
+        if (dyn is ExpandoObject)
+            return ((IDictionary<string, object>)dyn).ContainsKey(propertyname);
+
+        return dyn.GetType().GetProperty(propertyname) != null;
+    }
+}

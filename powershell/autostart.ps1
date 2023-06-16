0d7459e95531af5fd095daf714b588c86e6c276e
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2023-06-16 13:03:09 +0200

    cleanup

--- powershell/autostart.ps1
+++ powershell/autostart.ps1
@@ -1,6 +1,8 @@
-# change alias to new ls
-Remove-Alias ls
-New-Alias -Name 'ls' -Value 'list'
+if(Get-Command "list" -erroraction 'silentlycontinue'){
+    # change alias to new ls
+    Remove-Alias ls
+    New-Alias -Name 'ls' -Value 'list'
+}
 
 # powershell ssh tools
 . "$PSScriptRoot\profile-snippet-sshtools.ps1"

2022a2981368684a5c9c2f67a7673b787719870d
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2023-09-19 09:31:25 +0200

    added missing class

---
+++ ConsoleUtils/ConsoleUtilsCore/AssemblyHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+
+
+public static class AssemblyHelper
+{
+    public static string GetNETVersionFromAssembly(string path)
+    {
+            
+        string result = null;
+        try
+        {
+            
+            var asm = System.Reflection.Assembly.LoadFrom(path);
+            object[] list = asm.GetCustomAttributes(true);
+            var attribute = list.OfType<TargetFrameworkAttribute>().First();
+
+            //Console.WriteLine(attribute.FrameworkName);
+            return attribute.FrameworkDisplayName;
+                
+        }
+        catch
+        {
+
+        }
+        return result;
+    }
+}
+
